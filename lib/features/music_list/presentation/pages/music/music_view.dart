c4b56f01b34674a7c2615d707a6caaadd7808848
AdriF31 <adrifatwa31@gmail.com>
2023-11-07 14:50:14 +0700

    feat(music view): add music controller ui

--- lib/features/music_list/presentation/pages/music/music_view.dart
+++ lib/features/music_list/presentation/pages/music/music_view.dart
@@ -4,6 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:music_player/features/music_list/domain/entities/music_entity.dart';
 import 'package:music_player/features/music_list/presentation/bloc/music/music_bloc.dart';
 import 'package:music_player/features/music_list/presentation/bloc/music_player/music_player_bloc.dart';
 import 'package:music_player/features/music_list/presentation/dto/music_dto.dart';
@@ -26,6 +27,145 @@ class MusicView extends StatelessWidget {
       //         fontFamily: 'gilroy', fontSize: 24, fontWeight: FontWeight.bold),
       //   ),
       // ),
+      floatingActionButton:BlocBuilder<MusicBloc, MusicState>(
+        builder: (context, state) {
+          if (state is OnSuccessGetMusic) {
+            return BlocBuilder<MusicPlayerBloc, MusicPlayerState>(
+              bloc: bloc,
+              builder: (context, s) {
+                if (s is OnMusicPlayed) {
+                  return GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MusicControllerPage(
+                                bloc: bloc,
+                                music: state.data,
+                                dto: MusicDTO(
+                                    index: bloc.currentSongIndex,
+                                    title: state
+                                        .data
+                                        ?.results?[s.currentSongIndex ?? 0
+                                    ]
+                                        .trackName,
+                                    image: state
+                                        .data
+                                        ?.results?[s.currentSongIndex ?? 0
+                                    ]
+                                        .image,
+                                    artist: state
+                                        .data
+                                        ?.results?[s.currentSongIndex ?? 0
+                                    ].artistName,
+                                    musicUrl: state
+                                        .data
+                                        ?.results?[s.currentSongIndex ?? 0
+                                    ]
+                                        .previewUrl),
+                              )));
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      height: 100,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          boxShadow: [BoxShadow(
+                              color: greyColor
+                          )]
+                      ),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: CachedNetworkImage(
+                              imageUrl: state
+                                  .data
+                                  ?.results?[s.currentSongIndex ?? 0
+                              ]
+                                  .image ??
+                                  "https://",
+                              width: 80,
+                              height: 80,
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Text(state
+                                          .data
+                                          ?.results?[s.currentSongIndex!]
+                                          .trackName ??
+                                          "", style: const TextStyle(
+                                          fontFamily: 'gilroy',
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold),
+                                        overflow: TextOverflow.ellipsis,),
+                                      Text(state
+                                          .data
+                                          ?.results?[s.currentSongIndex!]
+                                          .artistName ??
+                                          "", style: const TextStyle(
+                                          fontFamily: 'gilroy',
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold),
+                                        overflow: TextOverflow.ellipsis,)
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      IconButton(
+                                          onPressed: () {
+                                            bloc.add(OnPreviousMusic());
+                                          },
+                                          icon: Icon(FluentIcons.previous_24_filled)),
+                                      IconButton(
+                                          onPressed: () {
+                                            s.isPlaying!?bloc.add(OnPauseMusic()):
+                                            bloc.add(OnResumeMusic());
+                                          },
+                                          icon: Icon((s.isPlaying!)
+                                              ? FluentIcons.pause_24_filled
+                                              : s is OnMusicStop
+                                              ? FluentIcons.play_24_filled
+                                              : FluentIcons.play_24_filled)),
+                                      IconButton(
+                                          onPressed: () {
+                                            bloc.add(OnNextMusic());
+                                          },
+                                          icon: Icon(FluentIcons.next_24_filled)),
+                                    ],
+                                  ),
+                                )
+
+                              ],
+                            ),
+                          ),
+
+                          // IconButton(onPressed: (){}, icon: Icon(FluentIcons.next_24_filled)),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                return SizedBox.shrink();
+              },
+            );
+          }
+          return SizedBox();
+        },
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16),
@@ -40,7 +180,9 @@ class MusicView extends StatelessWidget {
               ),
               BlocConsumer<MusicBloc, MusicState>(
                 listener: (context, state) {
-                  // TODO: implement listener
+                  if (state is OnSuccessGetMusic) {
+                    bloc.add(OnLoadMusic(musicList: state.musicList));
+                  }
                 },
                 builder: (context, state) {
                   if (state is OnSuccessGetMusic) {
@@ -56,16 +198,20 @@ class MusicView extends StatelessWidget {
                                 _buildMusicList(
                                     index: index,
                                     bloc: bloc,
-                                    image: state.data?.results?[index].image,
-                                    title:
-                                        state.data?.results?[index].trackName,
-                                    singer:
-                                        state.data?.results?[index].artistName,
-                                    musicUrl:
-                                        state.data?.results?[index].previewUrl),
+                                    music: state.data,
+                                    image:
+                                        state.data?.results?[index].image,
+                                    title: state
+                                        .data?.results?[index].trackName,
+                                    singer: state
+                                        .data?.results?[index].artistName,
+                                    musicUrl: state
+                                        .data?.results?[index].previewUrl),
                               if (state.data?.results?[index].previewUrl !=
-                              null&&index !=
-                                  (state.data?.results?.length ?? 0) - 1)
+                                      null &&
+                                  index !=
+                                      (state.data?.results?.length ?? 0) -
+                                          1)
                                 const Divider(
                                   thickness: 2,
                                   color: dividerColor,
@@ -76,7 +222,7 @@ class MusicView extends StatelessWidget {
                       ),
                     );
                   }
-                  if(state is OnLoadingGetMusic){
+                  if (state is OnLoadingGetMusic) {
                     return const Expanded(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -88,14 +234,16 @@ class MusicView extends StatelessWidget {
                       ),
                     );
                   }
-               return const Expanded(
-                 child: Column(
-                   mainAxisAlignment: MainAxisAlignment.center,
-                   children: [
-                     Center(child: Text("Silahkan Cari Musik Yang anda suka"),),
-                   ],
-                 ),
-               );
+                  return const Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: Text("Silahkan Cari Musik Yang anda suka"),
+                        ),
+                      ],
+                    ),
+                  );
                 },
               ),
             ],
@@ -145,6 +293,7 @@ class MusicView extends StatelessWidget {
 
   Widget _buildMusicList(
       {required MusicPlayerBloc bloc,
+        MusicEntity? music,
       String? image,
       String? title,
       String? singer,
@@ -163,8 +312,9 @@ class MusicView extends StatelessWidget {
                 MaterialPageRoute(
                     builder: (context) => MusicControllerPage(
                           bloc: bloc,
+                          music: music,
                           dto: MusicDTO(
-                            index: index,
+                              index: index,
                               title: title,
                               image: image,
                               artist: singer,
@@ -215,32 +365,29 @@ class MusicView extends StatelessWidget {
                 ),
                 IconButton(
                     key: key,
-                    onPressed:
-                        (state is OnMusicPlayed || state is OnMusicResumed) &&
+                    onPressed: (state is OnMusicPlayed&&state.isPlaying!) &&
+                            bloc.currentSongIndex == index
+                        ? () async {
+                            bloc.add(OnPauseMusic());
+                          }
+                        : state is OnMusicPlayed&&state.isPlaying==false&&
                                 bloc.currentSongIndex == index
-                            ? () async {
-                                bloc.add(OnPauseMusic());
+                            ? () {
+                                bloc.add(OnResumeMusic());
                               }
-                            : state is OnMusicPaused &&
-                                    bloc.currentSongIndex == index
-                                ? () {
-                                    bloc.add(OnResumeMusic());
-                                  }
-                                : () {
-                                    print(musicUrl);
-                                    print("key: ${bloc.currentSongIndex}");
-                                    print("index: $index");
-                                    bloc.add(OnPlayMusic(
-                                        musicUrl: musicUrl ?? "",
-                                        index: index));
-                                  },
-                    icon: Icon(
-                        (state is OnMusicPlayed || state is OnMusicResumed) &&
-                                bloc.currentSongIndex == index
-                            ? FluentIcons.pause_24_filled
-                            : state is OnMusicStop
-                                ? FluentIcons.play_24_filled
-                                : FluentIcons.play_24_filled))
+                            : () {
+                                print(musicUrl);
+                                print("key: ${bloc.currentSongIndex}");
+                                print("index: $index");
+                                bloc.add(OnPlayMusic(
+                                    musicUrl: musicUrl ?? "", index: index));
+                              },
+                    icon: Icon((state is OnMusicPlayed&&state.isPlaying!) &&
+                            bloc.currentSongIndex == index
+                        ? FluentIcons.pause_24_filled
+                        : state is OnMusicStop
+                            ? FluentIcons.play_24_filled
+                            : FluentIcons.play_24_filled))
               ],
             ),
           ),
