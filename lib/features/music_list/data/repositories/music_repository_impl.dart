a150e5eb88d5264ba45bda1093d1b578c162426b
AdriF31 <adrifatwa31@gmail.com>
2023-11-08 16:39:15 +0700

    ref(logging): add logging

--- lib/features/music_list/data/repositories/music_repository_impl.dart
+++ lib/features/music_list/data/repositories/music_repository_impl.dart
@@ -7,6 +7,7 @@ import 'package:music_player/core/error/failure.dart';
 import 'package:music_player/features/music_list/data/models/music_model.dart';
 import 'package:music_player/features/music_list/domain/entities/music_entity.dart';
 import 'package:music_player/features/music_list/domain/repositories/music_repository.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class MusicRepositoryImpl implements MusicRepository {
   Dio dio = Dio();
@@ -14,7 +15,14 @@ class MusicRepositoryImpl implements MusicRepository {
   @override
   Future<Either<Failure, MusicEntity>> searchMusic(String? term) async {
     try {
-      dio.interceptors.add(LogInterceptor(request: true,responseBody: true));
+      dio.interceptors.add(PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseBody: true,
+          responseHeader: false,
+          error: true,
+          compact: true,
+          maxWidth: 140));
       var res = await dio.get(baseUrl, queryParameters: {"term": term,"limit":20,"entity":"song"});
       if (res.statusCode == 200) {
         var data = MusicModel.fromJson(jsonDecode(res.data));
