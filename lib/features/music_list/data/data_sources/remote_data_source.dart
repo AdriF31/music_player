913add2f57ff5a606b3c89532130c1cb284e271f
AdriF31 <adrifatwa31@gmail.com>
2023-11-13 12:06:48 +0700

    feat(data): add remote data source

---
+++ lib/features/music_list/data/data_sources/remote_data_source.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:music_player/const/api_path.dart';
+import 'package:music_player/core/error/exception.dart';
+import 'package:music_player/core/network.dart';
+import 'package:music_player/features/music_list/data/models/music_model.dart';
+
+abstract class MusicRemoteDataSource{
+  Future<MusicModel> getMusic(String? term);
+}
+
+class MusicRemoteDataSourceImpl extends MusicRemoteDataSource{
+  Network network = Network();
+  MusicRemoteDataSourceImpl({required this.network});
+  @override
+  Future<MusicModel> getMusic(String? term) async{
+    var res = await network.dio.get(baseUrl, queryParameters: {"term": term,"limit":20,"entity":"song"});
+    if (res.statusCode == 200) {
+      return MusicModel.fromJson(jsonDecode(res.data));
+    } else {
+      throw ServerException();
+    }
+  }
+
+}
