7a4c2368855182806f192b37469723a2bbfb67c4
AdriF31 <adrifatwa31@gmail.com>
2023-11-13 12:07:30 +0700

    feat(data): add music repository impl

--- lib/features/music_list/data/repositories/music_repository_impl.dart
+++ lib/features/music_list/data/repositories/music_repository_impl.dart
@@ -1,37 +1,32 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:music_player/const/api_path.dart';
+import 'package:music_player/core/error/exception.dart';
 import 'package:music_player/core/error/failure.dart';
+import 'package:music_player/core/network.dart';
+import 'package:music_player/di/injection.dart';
+import 'package:music_player/features/music_list/data/data_sources/remote_data_source.dart';
 import 'package:music_player/features/music_list/data/models/music_model.dart';
 import 'package:music_player/features/music_list/domain/entities/music_entity.dart';
 import 'package:music_player/features/music_list/domain/repositories/music_repository.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class MusicRepositoryImpl implements MusicRepository {
-  Dio dio = Dio();
+  MusicRemoteDataSource musicRemoteDataSource=MusicRemoteDataSourceImpl(network: sl());
 
   @override
   Future<Either<Failure, MusicEntity>> searchMusic(String? term) async {
     try {
-      dio.interceptors.add(PrettyDioLogger(
-          requestHeader: true,
-          requestBody: true,
-          responseBody: true,
-          responseHeader: false,
-          error: true,
-          compact: true,
-          maxWidth: 140));
-      var res = await dio.get(baseUrl, queryParameters: {"term": term,"limit":20,"entity":"song"});
-      if (res.statusCode == 200) {
-        var data = MusicModel.fromJson(jsonDecode(res.data));
-        return Right(data);
-      } else {
-        return Left(ServerFailure(res.statusMessage));
-      }
-    } catch (e) {
-      rethrow;
+    var res = await musicRemoteDataSource.getMusic(term);
+    return Right(res);
+    } on ServerException{
+      return Left(ServerFailure("An error has Occured"));
+    }
+    on SocketException {
+      return Left(ConnectionFailure("Failed to connect to the network"));
     }
   }
 }
